da0636c2925cf3b348ddcdb144344ceb3948224b
admodev <admosdigital@yahoo.com>
2021-02-13 11:54:01 -0300

    config dotenv

--- backend/server.php
+++ backend/server.php
@@ -1,3 +1,16 @@
 <?php
 
-echo "placeholder";
+// Load variables from dotenv
+require_once './vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable('./');
+$dotenv->load();
+
+$db_host = $_ENV['DATABASE_HOST'];
+$db_user = $_ENV['DATABASE_USER'];
+$db_password = $_ENV['DATABASE_PASSWORD'];
+$db_name = $_ENV['DATABASE_NAME'];
+$db_port = $_ENV['DATABASE_PORT'];
+
+// MySQL connection using dotenv
+$database = mysqli_connect($db_host, $db_user, $db_password, $db_name, $db_port);
